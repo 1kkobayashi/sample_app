614732b1739979de96dfb663cef5955fd862a962
小林憲哉 <kobayashikenya@kobayashikennarinoMacBook-Air.local>
2019-01-24 20:36:06 +0900

    homeとhelpページのテスト

---
+++ spec/controllers/static_pages_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe StaticPagesController, type: :controller do
+  describe "#home" do
+    it "homeページにアクセスできる" do
+      get :home
+      expect(response.status).to eq(200)
+    end
+  end
+
+  describe "#help" do
+    it "helpページにアクセスできる" do
+      get :help
+      expect(response.status).to eq(200)
+    end
+  end
+end
