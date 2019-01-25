aec8eeb57277488738e9a6b0a679d4edb6272d72
小林憲哉 <kobayashikenya@kobayashikennarinoMacBook-Air.local>
2019-01-25 12:31:59 +0900

    3.4.2 タイトルを追加する (Green)

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,11 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
+  render_views
+
+  let(:base_title) { 'Ruby on Rails Tutorial Sample App' }
+
   describe "#home" do
     it "homeページにアクセスできる" do
       get :home
       expect(response.status).to eq(200)
-      assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+      assert_select "title", "Home | #{base_title}"
     end
   end
 
@@ -13,7 +17,7 @@ RSpec.describe StaticPagesController, type: :controller do
     it "helpページにアクセスできる" do
       get :help
       expect(response.status).to eq(200)
-      assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+      assert_select "title", "Help | #{base_title}"
     end
   end
 
@@ -21,7 +25,8 @@ RSpec.describe StaticPagesController, type: :controller do
     it "aboutページにアクセスする" do
      get :about
      expect(response.status).to eq(200)
-     assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+     assert_select "title", "About | #{base_title}"
     end
   end
+
 end
