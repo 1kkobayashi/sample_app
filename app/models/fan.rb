fef950a82114a8551dfa08de00bae43d6942f417
小林憲哉 <kobayashikenya@kobayashikennarinoMacBook-Air.local>
2019-01-31 22:07:12 +0900

    1.31_練習2

--- app/models/fan.rb
+++ app/models/fan.rb
@@ -8,7 +8,15 @@ class Fan < ApplicationRecord
   enum gender: { male: 0, female: 1 }
 
   # NOTE: 誕生日と年齢が確からしいかvalidateメソッドを作って確認してみよう
+  validate :age_equal_to_birthday
 
+  def age_equal_to_birthday
+    if (Date.today.strftime('%Y%m%d').to_i - birthday.strftime('%Y%m%d').to_i)/10000.floor == age
+      return nil
+    else
+      errors.add(:birthday, "incorrect with age")
+    end
+  end
   # NOTE: fanのフルネームを`fan.full_name`で取得できるようにしてみよう
   def full_name
     first_name + " " + last_name
